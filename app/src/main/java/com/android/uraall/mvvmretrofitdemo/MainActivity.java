353fed661c21efa8190842ac136662c238b57a0e
YouRa <allakhverdovyuriy@gmail.com>
2020-03-04 15:15:31 +0200

    Create RecyclerView

--- app/src/main/java/com/android/uraall/mvvmretrofitdemo/MainActivity.java
+++ app/src/main/java/com/android/uraall/mvvmretrofitdemo/MainActivity.java
@@ -1,17 +1,29 @@
 package com.android.uraall.mvvmretrofitdemo;
 
+import android.content.res.Configuration;
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.DefaultItemAnimator;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import com.android.uraall.mvvmretrofitdemo.adapter.ResultAdapter;
 import com.android.uraall.mvvmretrofitdemo.model.MovieApiResponse;
+import com.android.uraall.mvvmretrofitdemo.model.Result;
 import com.android.uraall.mvvmretrofitdemo.service.MovieApiService;
 import com.android.uraall.mvvmretrofitdemo.service.RetrofitInstance;
+
+import java.util.ArrayList;
 
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ArrayList<Result> results;
+    private RecyclerView recyclerView;
+    private ResultAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,6 +47,15 @@ public class MainActivity extends AppCompatActivity {
                 MovieApiResponse movieApiResponse =
                         response.body();
 
+                if (movieApiResponse != null &&
+                        movieApiResponse.getResults() != null) {
+
+                    results = (ArrayList<Result>) movieApiResponse
+                            .getResults();
+                    fillRecyclerView();
+
+                }
+
             }
 
             @Override
@@ -44,5 +65,28 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+    }
+
+    private void fillRecyclerView() {
+
+        recyclerView = findViewById(R.id.recyclerView);
+        adapter = new ResultAdapter(this, results);
+
+        if (getResources().getConfiguration().orientation ==
+                Configuration.ORIENTATION_PORTRAIT) {
+
+            recyclerView.setLayoutManager(
+                    new GridLayoutManager(this, 2));
+
+        } else {
+
+            recyclerView.setLayoutManager(
+                    new GridLayoutManager(this, 4));
+
+        }
+
+        recyclerView.setItemAnimator(new DefaultItemAnimator());
+        recyclerView.setAdapter(adapter);
+        adapter.notifyDataSetChanged();
     }
 }
---
+++ app/src/main/java/com/android/uraall/mvvmretrofitdemo/adapter/ResultAdapter.java
@@ -0,0 +1,73 @@
+package com.android.uraall.mvvmretrofitdemo.adapter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.android.uraall.mvvmretrofitdemo.R;
+import com.android.uraall.mvvmretrofitdemo.model.Result;
+import com.bumptech.glide.Glide;
+
+import java.util.ArrayList;
+
+public class ResultAdapter
+        extends RecyclerView.Adapter<ResultAdapter.ResultViewHolder> {
+
+    private Context context;
+    private ArrayList<Result> results;
+
+    public ResultAdapter(Context context, ArrayList<Result> results) {
+        this.context = context;
+        this.results = results;
+    }
+
+    @NonNull
+    @Override
+    public ResultViewHolder onCreateViewHolder(@NonNull ViewGroup parent,
+                                               int viewType) {
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.result_list_item, parent, false);
+
+        return new ResultViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ResultViewHolder holder, int position) {
+
+        holder.titleTextView.setText(results.get(position).getOriginalTitle());
+        holder.popularityTextView.setText(Double.toString(results.get(position)
+        .getPopularity()));
+        String imagePath = "https://image.tmdb.org/t/p/w500/" +
+                results.get(position).getPosterPath();
+        Glide.with(context)
+                .load(imagePath)
+                .into(holder.movieImageView);
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return results.size();
+    }
+
+    public class ResultViewHolder extends RecyclerView.ViewHolder {
+
+        public TextView titleTextView;
+        public TextView popularityTextView;
+        public ImageView movieImageView;
+
+        public ResultViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            titleTextView = itemView.findViewById(R.id.titleTextView);
+            popularityTextView = itemView.findViewById(R.id.popularityTextView);
+            movieImageView = itemView.findViewById(R.id.movieImageView);
+        }
+    }
+}
