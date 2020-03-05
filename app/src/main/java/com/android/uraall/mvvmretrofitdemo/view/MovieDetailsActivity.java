09d8a4699bbb94a63e05f6bc5d734c08f0dfa521
YouRa <allakhverdovyuriy@gmail.com>
2020-03-05 13:11:11 +0200

    Get all data

--- app/src/main/java/com/android/uraall/mvvmretrofitdemo/view/MovieDetailsActivity.java
+++ app/src/main/java/com/android/uraall/mvvmretrofitdemo/view/MovieDetailsActivity.java
@@ -4,19 +4,32 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.android.uraall.mvvmretrofitdemo.R;
 import com.android.uraall.mvvmretrofitdemo.model.Result;
+import com.bumptech.glide.Glide;
 
 public class MovieDetailsActivity extends AppCompatActivity {
 
     private Result result;
+    private ImageView posterImageView;
+    private String posterPath;
+    private TextView titleTextView;
+    private TextView voteCountTextView;
+    private TextView overviewTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_movie_details);
+
+        posterImageView = findViewById(R.id.imageView);
+        titleTextView = findViewById(R.id.titleTextView);
+        voteCountTextView = findViewById(R.id.voteCountTextView);
+        overviewTextView = findViewById(R.id.overviewTextView);
 
         Intent intent = getIntent();
 
@@ -26,6 +39,17 @@ public class MovieDetailsActivity extends AppCompatActivity {
 
             Toast.makeText(this, result.getOriginalTitle(),
                     Toast.LENGTH_LONG).show();
+            posterPath = result.getPosterPath();
+            String imagePath = "https://image.tmdb.org/t/p/w500/" + posterPath;
+
+            Glide.with(this)
+                    .load(imagePath)
+                    .placeholder(R.drawable.progress_circle)
+                    .into(posterImageView);
+
+            titleTextView.setText(result.getOriginalTitle());
+            voteCountTextView.setText(Integer.toString(result.getVoteCount()));
+            overviewTextView.setText(result.getOverview());
 
         }
 
